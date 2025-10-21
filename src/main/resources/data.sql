c3617a40116ed6857c21f858bb68b7ae8c3f96aa
sangamesh <sangameshgadedqe@gmail.com>
2025-10-21 22:49:16 +0530

    fixing the code

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO PRODUCTS (id, name, description, price, stock) VALUES (1, 'Basic T-Shirt', 'Comfortable cotton T-shirt', 199.00, 50);
+INSERT INTO PRODUCTS (id, name, description, price, stock) VALUES (2, 'Sneakers', 'Running sneakers', 2999.00, 20);
+INSERT INTO PRODUCTS (id, name, description, price, stock) VALUES (3, 'Coffee Mug', 'Ceramic mug 350ml', 249.00, 100);
